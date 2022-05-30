2b3640e9e9c34723a9cbf995fd9e89ec66fa1d17
Iverick <iverick091@gmail.com>
2022-05-30 23:44:41 +0300

    Views column added to Post.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_161140) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_30_203659) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
