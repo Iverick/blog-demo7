a600359ce7f8587fe79aa965e1eddf12109441ad
Iverick <iverick091@gmail.com>
2022-06-01 12:13:07 +0300

    Added edit button to comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_url(@post), notice: 'Comment has been updated' }
+      else
+        format.html { redirect_to post_url(@post), alert: 'There was an error while updating the comment' }
+      end
+    end
+  end
+
   def destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
